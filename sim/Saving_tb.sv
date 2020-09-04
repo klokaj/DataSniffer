903c1d96e73def70e1c6b064ce501f2465bc55ab
klokaj <krzysiek.lokaj@gmail.com>
2020-09-04 13:12:30 +0200

    Update Saving_tb.sv

--- sim/Saving_tb.sv
+++ sim/Saving_tb.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company:     AGH-UST
+// Engineer:    Krzysztof £okaj
 // 
 // Create Date: 03.09.2020 17:28:32
 // Design Name: 
