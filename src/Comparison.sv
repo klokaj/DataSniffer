e64b0ee6a773857735e4ea8fb2082e33b6bc6eda
klokaj <krzysiek.lokaj@gmail.com>
2020-09-04 13:13:35 +0200

    Update Comparison.sv

--- src/Comparison.sv
+++ src/Comparison.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company:     AGH-UST
-// Engineer:    Krzysztof £okaj
+// Engineer:    Krzysztof Lokaj
 // 
 // Create Date: 03.09.2020 12:56:44
 // Design Name: 
